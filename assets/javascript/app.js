14801a17bc348eb7e590ebd97c4d4edeabb66aeb
Tom Hsu <thomaschsu@gmail.com>
2018-06-09 21:45:26 -0400

    Fixed append bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,10 @@
 // Current Bug List
 // 1. Fix Clear Results button
 // 2. Before search is pulled, clear any previous search results
-// 3. Fix append bug
 
 var articlecontainer = $("#article-container");
 var startYr = "";
 var endYr = "";
-var artdiv = $("<div>");
-var pheadline = $("<p>");
-var pbyline = $("<li>");
-var p_sec_nm = $("<li>");
-var p_pub_dt = $("<li>");
-var p_w_url = $("<p>");
 
 // Article container starts off as hidden
 articlecontainer.hide();
@@ -20,11 +13,11 @@ $(document).on("click", ".btn", function() {
 
     if ($(this).text() == " Search") {
 
-        if ($("#startyear").val() !== "") {
+        if ($("#startyear").val() != "") {
             startYr = "&begin_date=" + $("#startyear").val() + "0101";
         }
 
-        if ($("#endyear").val() !== "") {
+        if ($("#endyear").val() != "") {
             endYr = "&end_date=" + $("#endyear").val() + "1231";
         }
 
@@ -39,16 +32,22 @@ $(document).on("click", ".btn", function() {
             articlecontainer.show();
 
             for (i = 0; i < $("#records").val(); i++) {
+                var artdiv = $("<div>");
                 artdiv.addClass("artDiv");
+                var pheadline = $("<p>");
                 pheadline.append(response.response.docs[i].headline.main);
                 pheadline.addClass("pheadline");
                 artdiv.append(pheadline);
+                var pbyline = $("<li>");
                 pbyline.append(response.response.docs[i].byline.original);
                 artdiv.append(pbyline);
+                var p_sec_nm = $("<li>");
                 p_sec_nm.append(response.response.docs[i].section_name);
                 artdiv.append(p_sec_nm);
+                var p_pub_dt = $("<li>");
                 p_pub_dt.append(response.response.docs[i].pub_date);
                 artdiv.append(p_pub_dt);
+                var p_w_url = $("<p>");
                 p_w_url.append('<a href="' + response.response.docs[i].web_url + '">' + response.response.docs[i].web_url);
                 p_w_url.addClass("urlClass");
                 artdiv.append(p_w_url);
